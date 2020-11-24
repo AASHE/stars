17dc099e357fad23c2ed0f8407f4a3cb79143772
dspeck1 <dspeck@burwood.com>
2020-11-24 11:49:52 -0600

    updated celery dep

--- src/run-celery.sh
+++ src/run-celery.sh
@@ -8,4 +8,6 @@ celery -A stars worker --config stars/celery.py --loglevel=DEBUG &
 echo "after celery"
 
 #Start Celery Beat
-#celery beat --config stars/celery.py -A stars -s /var/www/stars/logs/beat.db --loglevel=DEBUG &
+celery beat --config stars/celery.py -A stars -s /var/www/stars/logs/beat.db --loglevel=DEBUG &
+
+echo "after celerybeat"
