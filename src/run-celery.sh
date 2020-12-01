6ebf268cecf222e603812521ce44d32d75bada88
dspeck1 <dspeck@burwood.com>
2020-12-01 13:18:45 -0600

    updated celery start script

--- src/run-celery.sh
+++ src/run-celery.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-echo "started"
-
 #Start Celery Workers
 celery -A stars worker --config stars/celery.py --loglevel=DEBUG &
 
-echo "after celery"
-
 #Start Celery Beat
-celery beat --config stars/celery.py -A stars -s /var/www/stars/logs/beat.db --loglevel=DEBUG &
-
-echo "after celerybeat"
+celery beat --config stars/celery.py -A stars -s /var/www/stars/logs/beat.db --loglevel=DEBUG
--- src/run-django.sh
+++ src/run-django.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-echo "started"
-
-
 # Migrate Django after each build
 python manage.py migrate
-
-echo "after migrate"
 
 # Collect website static assets
 python manage.py collectstatic --noinput
 
-echo "after collectstatic"
-
+# Start Django
 python manage.py runserver 0.0.0.0:8080 --noreload
 
-echo "after start django"
